4348ae983b22d5eb8df69b7a9592568312d0941e
Christiaan van der Merwe <christiaan@genasys.co.za>
2022-05-16 23:20:11 +0200

    TestOrderBookController | Create simple test for getOrderHistory

--- src/test/controllers/TestOrderBookController.kt
+++ src/test/controllers/TestOrderBookController.kt
@@ -1,17 +1,26 @@
 import controllers.IOrderBookController
 import controllers.OrderBookController
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
 import modules.providers.IOrderBookProvider
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import types.models.response.OrderBookResult
 
 class TestOrderBookController {
-  private val orderBookProviderMock = mockk<IOrderBookProvider>()
-  private val orderBookController: IOrderBookController = OrderBookController(orderBookProviderMock)
+  private val _orderBookProviderMock = mockk<IOrderBookProvider>()
+  private val _orderBookController: IOrderBookController = OrderBookController(_orderBookProviderMock)
 
   @Test
   fun getOrderHistory_CallsExpectedProviderMethod() {
-    assertThrows<NotImplementedError>{ orderBookController.getOrderBook(CurrencyPair.BTCZAR) }
+    val expectedResult: OrderBookResult = mockk()
+    every { _orderBookProviderMock.getOrderHistory(any()) } returns expectedResult
+
+    val result = _orderBookController.getOrderBook(CurrencyPair.BTCZAR)
+
+    verify { _orderBookController.getOrderBook(CurrencyPair.BTCZAR) }
+    assert(result == expectedResult)
   }
 }
 
